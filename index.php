87cd0e3a982da77c8c19cdecf3c64e00bbc25429
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-02-09 21:45:56 +0700

    komentar

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>for</title>
+</head>
+<body>
+<ol>
+<?php 
+for ($i=4;$i<=8;$i++) {
+    echo "<li> bilangan ke $i </li>";
+
+}
+?>
+</ol>   
+</body>
+</html>
